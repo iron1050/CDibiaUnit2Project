535e9068f7c642ad0d0797451b6fcc064e202188
BT_3W2_06 <c@>
2023-10-20 12:35:40 -0400

    working on equation

--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,19 +1,23 @@
+import javax.sound.sampled.Line;
 import java.util.Scanner;
 public class LinearEquationRunner {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.print("Enter a pair of coordinates: ");
-        String coord1 = s.next();
-        /*System.out.print("Enter another pair of coordinates: ");
-        String coord2 = s.next();
-        double rounded = Math.round(1.896 * 100 ) / 100.0;
-        System.out.print(rounded);*/
-
+        String coord1 = s.nextLine();
+        System.out.print("Enter another pair of coordinates: ");
+        String coord2 = s.nextLine();
+        //double rounded = Math.round(1.896 * 100 ) / 100.0;        IMPORTANT FOR ROUNDING LTR
+        //System.out.print(rounded);
         int x1 = Integer.parseInt(coord1, 1, coord1.indexOf(","), 10);
-        System.out.print(x1);
-        /*int y1 = Integer.parseInt(coord1, coord1.indexOf(",") + 2, coord1.indexOf(")"), 10);
-        System.out.print("\n" + y1);*/
-        System.out.println(coord1);
-        System.out.print("\n" + coord1.indexOf(")"));
+        int y1 = Integer.parseInt(coord1, coord1.indexOf(",") + 2, coord1.indexOf(")"), 10);
+        int x2 = Integer.parseInt(coord2, 1, coord2.indexOf(","), 10);
+        int y2 = Integer.parseInt(coord2, coord2.indexOf(",") + 2, coord2.indexOf(")"), 10);
+        LinearEquation test = new LinearEquation(x1, y1, x2, y2);
+        System.out.println(test.slope());
+        //System.out.println(test.equation());
+
+
+
     }
 }
