107a119c71871bd28ff07f99343e9788462d69de
Chukwuma Dibia <chukwumadibia@gmail.com>
2023-10-23 16:48:29 -0400

    finished

--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class LinearEquationRunner {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        System.out.println("Welcome!");
         System.out.print("Enter a pair of coordinates: ");
         String coord1 = s.nextLine();
         System.out.print("Enter another pair of coordinates: ");
@@ -14,24 +15,12 @@ public class LinearEquationRunner {
             System.out.println("This equation is a vertical line, with an equation of x = " + x1);
         }else {
             LinearEquation equation = new LinearEquation(x1, y1, x2, y2);
-            //System.out.println(test.slope());
-            //System.out.println(test.equation());
-        /*int x1 = 1;
-        int y1 = -10;
-        int x2 = -3;
-        int y2 = 2;*/
-
-        //LinearEquation equation = new LinearEquation(x1, y1, x2, y2);
-        System.out.println("Equation: " + equation.equation());
-        System.out.println("Slope: " + equation.slope());
-        System.out.println("y-intercept: " + equation.yIntercept());
-        System.out.println("Distance: " + equation.distance());
-        System.out.println();
-        System.out.println("----- Line info -----");
-        System.out.println(equation.lineInfo());
-        System.out.println();
-        double testX = 4;
-        System.out.println("Coordinate for x: " + equation.coordinateForX(testX));
+            System.out.println("----- Line info -----");
+            System.out.println(equation.lineInfo());
+            System.out.println();
+            System.out.print("Type an x coordinate to find the value of: ");
+            double testX = s.nextDouble();
+            System.out.println("Coordinate for x: " + equation.coordinateForX(testX));
 
 
         }
